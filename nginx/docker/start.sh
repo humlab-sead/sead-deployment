eefe06a36d3a72d67c414927c153aa4fd51b9475
jonlig04 <johan.von.boer@umu.se>
2022-05-05 14:23:02 +0200

    reworked structure

--- nginx/docker/start.sh
+++
@@ -1,9 +0,0 @@
-if [ $NO_SSL ]
-then
-	echo "Starting server without SSL"
-	/usr/sbin/nginx && sleep infinity
-else
-	echo "Starting server with SSL"
-	echo "Retrieving cert for domain $DOMAIN with email $ADMIN_EMAIL\n" && /usr/sbin/nginx && certbot --non-interactive --email $ADMIN_EMAIL --text --agree-tos --nginx --domains $DOMAIN && sleep infinity
-fi
-
