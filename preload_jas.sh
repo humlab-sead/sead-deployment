f672269670c87c7180cc3b81528c67335151de5c
Johan von Boer <johan.von.boer@umu.se>
2025-04-07 16:20:42 +0200

    added flushing of the graphcache

--- preload_jas.sh
+++ preload_jas.sh
@@ -3,9 +3,18 @@
 # Load .env
 export $(grep -v '^#' .env | xargs)
 
-# Define the endpoint
-URL="http://localhost:8080/jsonapi/rebuild"
+# Define the base URL using WEB_PORT from .env
+BASE_URL="http://localhost:${WEB_PORT}"
 
-# Make the request with Basic Auth
-curl -u "$JAS_PROTECTED_ENDPOINTS_USER:$JAS_PROTECTED_ENDPOINTS_PASS" "$URL"
+# Call /jsonapi/rebuild
+echo "Calling ${BASE_URL}/jsonapi/rebuild..."
+curl -u "$JAS_PROTECTED_ENDPOINTS_USER:$JAS_PROTECTED_ENDPOINTS_PASS" "${BASE_URL}/jsonapi/rebuild"
+
+echo -e "\n"
+
+# Call /flush/graphcache
+echo "Calling ${BASE_URL}/flush/graphcache..."
+curl -u "$JAS_PROTECTED_ENDPOINTS_USER:$JAS_PROTECTED_ENDPOINTS_PASS" "${BASE_URL}/jsonapi/flush/graphcache"
+
+echo -e "\nDone."
 
