d50c59cc475b90ed3f9cd399adeae65e9a78d264
Johan von Boer <johan.von.boer@umu.se>
2025-04-08 09:26:18 +0200

    added longer timeouts

--- preload_jas.sh
+++ preload_jas.sh
@@ -6,15 +6,17 @@ export $(grep -v '^#' .env | xargs)
 # Define the base URL using WEB_PORT from .env
 BASE_URL="http://localhost:${WEB_PORT}"
 
+# Set a long timeout since calling "rebuild" will take time
+TIMEOUT=3600
+
 # Call /jsonapi/rebuild
 echo "Calling ${BASE_URL}/jsonapi/rebuild..."
-curl -u "$JAS_PROTECTED_ENDPOINTS_USER:$JAS_PROTECTED_ENDPOINTS_PASS" "${BASE_URL}/jsonapi/rebuild"
+curl -m "$TIMEOUT" -u "$JAS_PROTECTED_ENDPOINTS_USER:$JAS_PROTECTED_ENDPOINTS_PASS" "${BASE_URL}/jsonapi/rebuild"
 
 echo -e "\n"
 
 # Call /flush/graphcache
 echo "Calling ${BASE_URL}/jsonapi/flush/graphcache..."
-curl -u "$JAS_PROTECTED_ENDPOINTS_USER:$JAS_PROTECTED_ENDPOINTS_PASS" "${BASE_URL}/jsonapi/flush/graphcache"
+curl -m "$TIMEOUT" -u "$JAS_PROTECTED_ENDPOINTS_USER:$JAS_PROTECTED_ENDPOINTS_PASS" "${BASE_URL}/jsonapi/flush/graphcache"
 
 echo -e "\nDone."
-
